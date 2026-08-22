0c2fda6661b748155dc2315ee3b436799dff71e2
Juan Ignacio Dominguez <juan.ignacio.dominguez.-nd@disney.com>
2026-08-22 14:42:36 -0300

    add bucket variable for terraform

--- src/terraform/environments/dev/backend.tf
+++ src/terraform/environments/dev/backend.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-dev-xxxxxxxx" # Will be updated after state bucket creation
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
