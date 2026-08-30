d683093f0c1328ff42d24739fa2b27876a9f3da8
Juan Ignacio Dominguez <juan.ignacio.dominguez.-nd@disney.com>
2026-08-30 15:26:56 -0300

    use terraform variables to define kubectl values

---
+++ src/terraform/environments/dev/outputs.tf
@@ -0,0 +1,14 @@
+output "app_config" {
+  sensitive = false
+  value = {
+    frontend_image        = var.frontend_image
+    backend_image         = var.backend_image
+    mysql_image           = var.mysql_image
+    frontend_cpu_limit    = var.frontend_cpu_limit
+    frontend_memory_limit = var.frontend_memory_limit
+    backend_cpu_limit     = var.backend_cpu_limit
+    backend_memory_limit  = var.backend_memory_limit
+    mysql_cpu_limit       = var.mysql_cpu_limit
+    mysql_memory_limit    = var.mysql_memory_limit
+  }
+}
